3e9fb8716d3be15709cee3bd3109f96fc712f64a
Hajter72 <126614372+Hajter72@users.noreply.github.com>
2025-03-18 02:15:11 +0100

    Ciag dalszy rozdzielenia

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from user.models import User
+
+
 # Create your views here.
+def users_list(request):
+    users = User.objects.all()
+    return render(request, 'user/user_list.html', {'users': users})
