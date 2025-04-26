9e6d30d08cbb621fd087cb045a049ad719e1a7c9
woznq <jakubw835@gmail.com>
2025-04-26 21:05:44 +0200

    Depozyt i wypłata do drobnych poprawek stylisycznych

--- frontend/src/components/Panel/Panel.jsx
+++ frontend/src/components/Panel/Panel.jsx
@@ -25,8 +25,13 @@ import creditCard from '../../assets//Panel/creditCard.png';
 import coinHand from '../../assets//Panel/coinHand.png';
 import coin from '../../assets/Panel/coin.png';
 
+import blikIcon from '../../assets/Panel/blik.png';
+import cardIcon from '../../assets/Panel/card.png';
+import paysafecardIcon from '../../assets/Panel/paysafecard.png';
+
 export function Panel({ isPanelOpen, closePanel }) {
   const [activePanel, setActivePanel] = useState('deposit');
+  const [activeDepositType, setActiveDepositType] = useState(null);
 
   if (!isPanelOpen) return null;
 
@@ -44,12 +49,47 @@ export function Panel({ isPanelOpen, closePanel }) {
           </button>
           <button onClick={() => setActivePanel('freeCoins')}>
             Darmowe monety
-             <img src={coin} alt="Darmowe monety" />
+            <img src={coin} alt="Darmowe monety" />
           </button>
         </div>
         <div className="right-panel">
-          {activePanel === 'deposit' && <h3>Depozyt (tu pojawi się zawartość)</h3>}
-          {activePanel === 'withdraw' && <h3>Wypłata (tu pojawi się zawartość)</h3>}
+          {activePanel === 'deposit' && (
+            <div className="deposit-options">
+              <button onClick={() => setActiveDepositType('blik')}>
+                Blik
+                <img src={blikIcon} alt="Blik" />
+              </button>
+              <button onClick={() => setActiveDepositType('card')}>
+                Karta
+                <img src={cardIcon} alt="Karta" />
+              </button>
+              <button onClick={() => setActiveDepositType('paysafecard')}>
+                Paysafecard
+                <img src={paysafecardIcon} alt="Paysafecard" />
+              </button>
+            </div>
+          )}
+          <div className="deposit-details">
+            {activeDepositType === 'blik' && <div>Blik (tu pojawi się zawartość)</div>}
+            {activeDepositType === 'card' && <div>Karta (tu pojawi się zawartość)</div>}
+            {activeDepositType === 'paysafecard' && <div>Paysafecard (tu pojawi się zawartość)</div>}
+          </div>
+          {activePanel === 'withdraw' && (
+            <div className="withdraw-section">
+              <h3>Wypłata na rachunek bankowy</h3>
+              <p>Wybierz numer rachunku do wypłaty z listy poniżej i wpisz kwotę wypłaty</p>
+              <div className="account-selection">
+                <label>Nr konta</label>
+                <input type="text" placeholder="Wpisz numer konta" />
+              </div>
+              <div className="amount-section">
+                <label>Kwota w zł</label>
+                <input type="number" />
+                <br />
+                <button>Wypłata</button>
+              </div>
+            </div>
+          )}
           {activePanel === 'freeCoins' && <h3>Darmowe monety (tu pojawi się zawartość)</h3>}
         </div>
       </div>
